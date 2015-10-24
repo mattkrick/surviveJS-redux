a052fae6484fb8b70c23c94b17364b1ae7713d06
mattkrick <matt.krick@gmail.com>
2015-10-24 17:47:36 -0500

    Chapter: react and flux (localStorage)

--- app/redux/store.jsx
+++ app/redux/store.jsx
@@ -3,20 +3,24 @@ import thunk from 'redux-thunk';
 import reducer from './reducers';
 import { devTools, persistState } from 'redux-devtools';
 import {addNote} from './actions';
+import localState from 'redux-localstorage'
 
 export default function configureStore() {
   const finalCreateStore = compose(
     applyMiddleware(thunk),
     // Provides support for DevTools:
     devTools(),
+    localState(),
     // Lets you write ?debug_session=<name> in address bar to persist debug sessions
     persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
   )(createStore);
 
   const store = finalCreateStore(reducer);
-  store.dispatch(addNote('Learn Webpack'));
-  store.dispatch(addNote('Learn React'));
-  store.dispatch(addNote('Do laundry'));
+  if (store.getState().notes.length === 0) {
+    store.dispatch(addNote('Learn Webpack'));
+    store.dispatch(addNote('Learn React'));
+    store.dispatch(addNote('Do laundry'));
+  }
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
